7ff77f6e19d455dfe64bd08f30dbc52411a3d8e7
limgandhi <lhyu123@naver.com>
2024-12-16 19:12:26 +0900

    Add firebase messaging code

--- src/global/FirebaseProvider.tsx
+++ src/global/FirebaseProvider.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren } from 'react';
 import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getMessaging, getToken } from 'firebase/messaging';
 import { FirebaseContext } from './FirebaseContext.ts';
 
 const FirebaseProvider = ({ children }: PropsWithChildren) => {
@@ -26,8 +27,23 @@ const FirebaseProvider = ({ children }: PropsWithChildren) => {
   const analytics = getAnalytics(app);
   // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
+  // Initialize Firebase Cloud Messaging and get a reference to the service
+  const messaging = getMessaging(app);
+  // requestPermission();
+  getToken(messaging, {
+    vapidKey: 'BA0rdpYCy7D2sktmp382c7fcfgOxJ1ukqpC7d9qanTaHgEuqXkd_4yNX9y4iNQO6pXl4MjjYXXK4JTfGXIO1iSQ',
+  }).then((currentToken) => console.log(currentToken));
 
   return <FirebaseContext.Provider value={{ db, analytics }}>{children}</FirebaseContext.Provider>;
 };
+
+function requestPermission() {
+  console.log('Requesting permission...');
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+    }
+  });
+}
 
 export default FirebaseProvider;
